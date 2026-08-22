17f081ada2789889915f88d2d741bd7c5ff91e1c
Agrima Saraf <agrimasaraf.runu@gmail.com>
2026-08-22 12:18:33 +0000

    Add overlap to transcript chunks

--- src/chunk_transcript.py
+++ src/chunk_transcript.py
@@ -15,25 +15,33 @@ sentences = re.split(r'(?<=[.!?])\s+', transcript)
 
 # Maximum size of each chunk
 max_chunk_size = 1000
+overlap_sentences = 2
 
 chunks = []
-current_chunk = ""
+current_sentences = []
+current_length = 0
 
-
-# Build chunks sentence by sentence
 for sentence in sentences:
+    sentence_length = len(sentence)
 
-    if len(current_chunk) + len(sentence) <= max_chunk_size:
-        current_chunk += sentence + " "
+    if current_length + sentence_length <= max_chunk_size:
+        current_sentences.append(sentence)
+        current_length += sentence_length
 
     else:
-        chunks.append(current_chunk.strip())
-        current_chunk = sentence + " "
+        chunks.append(" ".join(current_sentences))
+
+        # Carry the last 2 sentences into the next chunk
+        current_sentences = current_sentences[-overlap_sentences:]
+
+        current_sentences.append(sentence)
+
+        current_length = sum(len(s) for s in current_sentences)
+
+if current_sentences:
+    chunks.append(" ".join(current_sentences))
 
 
-# Add the final chunk
-if current_chunk:
-    chunks.append(current_chunk.strip())
 
 
 # Show the results
@@ -45,5 +53,5 @@ print(chunks[0])
 print("\nSECOND CHUNK:\n")
 print(chunks[1])
 
-print("\nSECOND CHUNK:\n")
-print(chunks[1])
+print("\nTHIRD CHUNK:\n")
+print(chunks[2])
